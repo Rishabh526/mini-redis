2d5a8664f2aaeaeb822fcb43e440d8ae86930cb4
Rishabh Rana <rishabh@Sukritis-MacBook-Air.local>
2026-05-30 14:48:24 +0530

    Implement TCP echo server and client

---
+++ client/client.py
@@ -0,0 +1,20 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 6379
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((HOST, PORT))
+
+while True:
+    message = input("Enter message: ")
+    client.send(message.encode())
+    response = client.recv(1024)
+    print("Server:", response.decode())
+    
+    if message.lower() == "exit":
+        break
+
+client.close()
+
---
+++ server/server.py
@@ -0,0 +1,32 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 6379
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((HOST, PORT))
+
+server.listen()
+
+print(f"Server is listening on {HOST}:{PORT}")
+
+client_socket, client_address = server.accept()
+
+print(f"Connected to {client_address}")
+
+while True:
+    data = client_socket.recv(1024)
+
+    if not data:
+        break
+
+    message = data.decode()
+
+    print(f"Received: {message}")
+
+    client_socket.send(data)
+
+client_socket.close()
+server.close()
+
